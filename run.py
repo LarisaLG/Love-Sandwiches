773703ab9d1236161507b352bef2ed993ffb93e9
LarisaLG <lgalisheva@gmail.com>
2022-07-01 15:39:22 +0000

    Add check that the entered values ​​are integers

--- run.py
+++ run.py
@@ -36,7 +36,9 @@ def validate_data(values):
     Raises ValueError f string cannot be converted into int,
     or if there aren't exactly 6 values.
     """
+    print(values)
     try:
+        [int(value) for value in values] 
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -45,6 +47,5 @@ def validate_data(values):
         print(f"Invalid data: {e}, please try again.\n")
 
 
-    print(values)
 
 get_sales_data()
